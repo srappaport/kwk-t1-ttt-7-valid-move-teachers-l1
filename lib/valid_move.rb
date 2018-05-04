8a66f4d826fbe8054290ae11e748229e9cd206cf
srappaport <sarah.rappaport@greendot.org>
2018-05-04 20:36:52 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,14 @@
 # code your #valid_move? method here
+def position_taken?(array, index)
+  if array[index] == " "
+    false
+  elsif array[index] == "" || array[index]== nil
+    false
+  else array[index]== "X" || array[index] == "O"
+    true
+  end
+end
+
 def valid_move?(array, index)
   if position_taken? == true && index.between?(0,8)
     true
